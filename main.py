9b92d0dcbaeee259d5e8e35cc6a1d7179048642c
bwifo <ben.morg06@gmail.com>
2023-07-01 16:48:47 +0100

    ep3 and 4 complete; tutorial finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame, neat, os, random, sys
+import pygame, neat, os, random, sys, pickle
+import math
 
 pygame.init()
 
@@ -30,6 +31,7 @@ class Dino:
         self.dino_jump = False
         self.rect = pygame.Rect(self.X_POS, self.Y_POS, self.image.get_width(), self.image.get_height())
         self.vel = self.JUMP_VEL
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.step_index = 0
     
     def update(self):
@@ -58,6 +60,9 @@ class Dino:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+        pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height),2)
+        for obstacle in obstacles:
+            pygame.draw.line(SCREEN, self.color, (self.rect.x+54, self.rect.y+12), obstacle.rect.center, 2)
 
 class Obstacle:
     def __init__(self,image, num_of_cacti):
@@ -87,27 +92,54 @@ class LargeCactus(Obstacle):
 
 def remove(index):
     dinos.pop(index)
+    ge.pop(index)
+    nets.pop(index)
 
+def distance(a, b):
+    dx = a[0] - b[0]
+    dy = a[1] - b[1]
+    return math.sqrt(dx*dx + dy*dy)
 
-def main():
-    global game_speed, x_pos_bg, y_pos_bg, points, dinos, obstacles
+def eval_genomes(genomes, config):
+    global game_speed, x_pos_bg, y_pos_bg, points, dinos, obstacles, ge, nets
     clock = pygame.time.Clock()
     points = 0
 
-    dinos = [Dino()]
+    dinos = []
     obstacles = []
+    ge = []
+    nets = []
 
     x_pos_bg = 0
     y_pos_bg = 380
     game_speed = 20
 
+    for genome_id, genome in genomes:
+        dinos.append(Dino())
+        ge.append(genome)
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+
     def score():
         global points, game_speed
         points += 1
+        for i, dino in enumerate(ge):
+            ge[i].fitness += 1
         if points % 100 == 0:
             game_speed += 1
         text = FONT.render(f"Score: {str(points)}", True, (0, 0, 0))
         SCREEN.blit(text, (950, 50))
+
+    def stats():
+        global dinos, game_speed, ge
+        text_1 = FONT.render(f"Dinosaurs Alive: {str(len(dinos))}", True, (0, 0, 0))
+        text_3 = FONT.render(f"Generations: {str(p.generation+1)}", True,(0,0,0))
+        text_2 = FONT.render(f"Game Speed: {str(game_speed)}", True, (0, 0, 0))
+
+        SCREEN.blit(text_1, (50, 450))
+        SCREEN.blit(text_3, (50, 480))
+        SCREEN.blit(text_2, (50, 510))
 
     #this function is used to move the background
     def background():
@@ -149,16 +181,43 @@ def main():
             obstacle.update()
             for i, dinosaur in enumerate(dinos):
                 if dinosaur.rect.colliderect(obstacle.rect):
+                    ge[i].fitness -=1
                     remove(i)
+        
+        for i, dino in  enumerate(dinos):
+            output = nets[i].activate((dino.rect.y, distance((dino.rect.x, dino.rect.y), obstacle.rect.midtop)))
+            if output[0] > 0.5 and dino.rect.y == dino.Y_POS:
+                dino.dino_jump = True
+                dino.dino_run = False
 
         
-        user_input = pygame.key.get_pressed()
-        if user_input[pygame.K_SPACE] and not dinos[0].dino_jump:
-            dinos[0].dino_jump = True
-            dinos[0].dino_run = False
+        # user_input = pygame.key.get_pressed()
+        # if user_input[pygame.K_SPACE]:
+        #     dinos[0].dino_jump = True
+        #     dinos[0].dino_run = False
 
+        stats()
         score()
         background()
         pygame.display.update()
 
-main()
+def run(config_path):
+    global p
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(eval_genomes, 50)
+    with open("best.pickle", "wb") as f:
+        pickle.dump(winner, f)
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    run(config_path)
